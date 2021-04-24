15569cdba7f42a85ccfab2a343f5499d0d00c63e
JoyMbugua <wacumbugua@gmail.com>
2021-04-24 15:49:18 +0300

    added migrations and password hashing

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from . import db
 from datetime import datetime
+from werkzeug.security import generate_password_hash,check_password_hash
 
 
 class User(db.Model):
@@ -7,11 +8,24 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(20), unique = True, index = True)
     email = db.Column(db.String(120), unique = True, index = True)
-    password = db.Column(db.String(60))
-    pitch = db.Relationship('Pitch', backref = 'user', lazy = 'dynamic')
+    password_hash = db.Column(db.String(255))
+    pitch = db.relationship('Pitch', backref = 'user', lazy = 'dynamic')
 
     def __repr__(self):
         return f"User('{self.username}', '{self.email}')"
+
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
+
+
+    def verify_password(self,password):
+        return check_password_hash(self.pass_secure,password)
+
 
 class Pitch(db.Model):
     id = db.Column(db.Integer, primary_key = True)
