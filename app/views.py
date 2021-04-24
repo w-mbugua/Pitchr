3dd1283bafd0f225c22f30553519ad474f4a6d13
JoyMbugua <wacumbugua@gmail.com>
2021-04-24 11:57:23 +0300

    added home page styling

--- app/views.py
+++ app/views.py
@@ -1,6 +1,17 @@
 from flask import render_template
 from app import app
 
+pitches = [
+    {'author': 'Sarah',
+    'date_posted': 'April 27 2021',
+    'pitch': 'I find the work your PR team does to be innovating and refreshing—I’d love the opportunity to put my expertise to work for your company…'
+    },
+    {
+        'author': 'Steve',
+        'date_posted': 'February 29, 2020',
+        'pitch': 'I’ve always been passionate about the way sports bring cultures together and would love the opportunity to bring my project management and leadership abilities to this position.'
+    }
+]
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', pitches = pitches)
