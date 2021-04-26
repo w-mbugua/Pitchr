715f264339fb10f9e16a524836519bc2fe18e7a3
JoyMbugua <wacumbugua@gmail.com>
2021-04-26 06:59:43 +0300

    created comment test

--- tests/test_comment.py
+++ tests/test_comment.py
@@ -1,16 +1,18 @@
 import unittest
-from app.models import User, Pitch
+from app.models import User, Comment, Pitch
 
-class PitchModelCase(unittest.TestCase):
+class CommentModelCase(unittest.TestCase):
     def setUp(self):
         self.user_Mary = User(username = 'Mary',password = 'testing', email = 'mary@testing.com')
         self.new_pitch = Pitch(post = 'some dummy pitch', user = self.user_Mary)
+        self.new_comment = Comment(pitch_id = 15, content = "some comment")
 
 
     def test_instance(self):
-        self.assertTrue(isinstance(self.new_pitch, Pitch))
+        self.assertTrue(isinstance(self.new_comment, Comment))
 
 
     def test_check_instance_variables(self):
-        self.assertEquals(self.new_pitch.post,'some dummy pitch')
-        self.assertEquals(self.new_pitch.user,self.user_Mary)
+        self.assertEquals(self.new_comment.content,'some comment')
+        self.assertEquals(self.new_comment.pitch_id, 15)
+
